97ac6cdd075110f719e94b35a6907797e0cd5f34
aussieyang <yang.j.li91@gmail.com>
2016-03-10 15:29:59 +1100

    basic features working

--- public/app.js
+++ public/app.js
@@ -2,7 +2,6 @@
 
   console.log('running');
 
-
   'use strict'
 
   // Namespacing the socket code under IO
@@ -18,6 +17,10 @@
     bindEvents : function() {
       IO.socket.on('connected', IO.onConnected );
       IO.socket.on('newGameCreated', IO.onNewGameCreated );
+      IO.socket.on('playerJoinedRoom', IO.playerJoinedRoom );
+      IO.socket.on('beginNewGame', IO.beginNewGame );
+      IO.socket.on('newRiddleData', IO.onNewRiddleData);
+      IO.socket.on('hostCheckAnswer', IO.hostCheckAnswer);
     },
 
     // Client connected
@@ -32,6 +35,30 @@
       App.Host.gameInit(data);
     },
 
+    // Player joins behaviour
+    playerJoinedRoom : function(data) {
+      // Update waiting screen function called for Player and Host
+      App[App.myRole].updateWaitingScreen(data);
+    },
+
+    // Call Countdown
+    beginNewGame : function(data) {
+      App[App.myRole].gameCountdown(data);
+    },
+
+    // Next Riddle Handler
+    onNewRiddleData : function(data) {
+      // Update the current round
+      App.currentRound = data.round;
+      // Change the word for the Host and Player
+      App[App.myRole].newRiddle(data);
+    },
+
+    hostCheckAnswer : function(data) {
+      if(App.myRole === 'Host') {
+          App.Host.checkAnswer(data);
+      }
+    },
 
   }
 
@@ -80,16 +107,18 @@
     bindEvents: function () {
       // For Host
       App.$doc.on('click', '#createGameBtn', App.Host.onCreateClick);
-      App.$doc.on('click', '#startGameBtn' /*Go to Game Page*/);
+      // App.$doc.on('click', '#startGameBtn' /*Go to Game Page*/);
 
       // For Player
       App.$doc.on('click', '#joinGameBtn', App.Player.onJoinClick);
       App.$doc.on('click', '#readyBtn', App.Player.onReadyClick);
-      App.$doc.on('click', '#submitAnswerBtn' /*Check answer and if correct move on; if not show tryAgain*/);
+      App.$doc.on('click', '#submitAnswerBtn', App.Player.onSubmitClick);
     },
 
 
+    // =======================
     // Namespacing Host Code
+    // =======================
     Host : {
       // Referencing Player data
       players : [],
@@ -115,6 +144,7 @@
         App.Host.displayInstructionView();
       },
 
+      // Showing Instruction page and appending elements to DOM
       displayInstructionView : function() {
         // Display Instruction template
         App.$gameBoard.html(App.$templateCreate);
@@ -124,10 +154,91 @@
         $('#gameId').text(App.gameId);
       },
 
+      // Pushing 'player joined' msg to Intruction page
+      updateWaitingScreen: function(data) {
+          // Update host screen
+          $('#playersWaiting')
+              .append('<p/>')
+              .text('Player ' + data.playerName + ' joined the game.');
+              console.log(data.playerName);
+          // Store the new player's data on the Host.
+          App.Host.players.push(data);
+          console.log(data);
+          // Increment the number of players in the room
+          App.Host.numPlayers += 1;
+          // If four players have joined, start the game!
+          if (App.Host.numPlayers === 4) {
+              // Let the server know room full.
+              IO.socket.emit('hostRoomFull',App.gameId);
+          }
+      },
+
+      // Countdown begins when room is full
+      gameCountdown : function() {
+          // Prepare the game screen with new HTML
+          App.$gameBoard.html(App.$templateGame);
+          // Begin the on-screen countdown timer
+          var $secondsLeft = $('#countdownTimer');
+          App.countDown( $secondsLeft, 5, function(){
+              console.log('Countdown finished');
+              IO.socket.emit('countdownFinished', App.gameId);
+          });
+          // Set the Name section for each player.
+          $('#player1Score').find('.playerName').text(App.Host.players[0].playerName + ': ');
+          $('#player2Score').find('.playerName').text(App.Host.players[1].playerName + ': ');
+          $('#player3Score').find('.playerName').text(App.Host.players[2].playerName + ': ');
+          $('#player4Score').find('.playerName').text(App.Host.players[3].playerName + ': ');
+
+          // Set the Score section on screen to 0 for each player.
+          $('#player1Score').find('.score').attr('id', App.Host.players[0].mySocketId.replace(/^.#+/, ''));
+          $('#player2Score').find('.score').attr('id', App.Host.players[1].mySocketId.replace(/^.#+/, ''));
+          $('#player3Score').find('.score').attr('id', App.Host.players[2].mySocketId.replace(/^.#+/, ''));
+          $('#player4Score').find('.score').attr('id', App.Host.players[3].mySocketId.replace(/^.#+/, ''));
+      },
+
+      // Behaviour for new round
+      newRiddle : function(data) {
+          console.log('newRiddle called');
+          // Insert the new riddle data into the DOM
+          $('#countdownTimer').text('Round ' + data.round);
+          $('#gameRiddle').text(data.riddle);
+          $('#gameQuestion').text(data.question);
+          // Update the data for the current round
+          App.Host.currentCorrectAnswer = data.answer;
+          App.Host.currentRound = data.round;
+      },
+
+
+      checkAnswer : function(data) {
+        // Verify that the answer clicked is from the current round. Stops late entries.
+        if (data.round == App.currentRound){
+          // Reference for current player score
+          var $pScore = $('#' + data.playerId);
+          // Check if correct
+          if( App.Host.currentCorrectAnswer == data.answer ) {
+            // Increment the player's score
+            $pScore.text(+$pScore.text() + 1);
+            // Advance the round
+            App.currentRound += 1;
+            // Prepare data to send to the server
+            var data = {
+                gameId : App.gameId,
+                round : +App.currentRound - 1
+            }
+            console.log(data);
+            // Notify the server to start the next round.
+            IO.socket.emit('hostNextRound', data);
+            } else {
+            // Emit to server
+          }
+        }
+      },
+
     },
 
-
+    // =======================
     // Namespacing Player Code
+    // =======================
     Player : {
       // Referencing Host's Socket ID
       hostSocketId: '',
@@ -138,7 +249,7 @@
       // Handler for clicking Join button
       onJoinClick: function () {
           // Display the Join Game view.
-          App.$gameArea.html(App.$templateJoin);
+          App.$gameBoard.html(App.$templateJoin);
       },
 
       // Handler for clicking Ready button
@@ -146,25 +257,94 @@
          // collect data to send to the server
          var data = {
              gameId : +($('#gameIdInput').val()),
-             playerName : $('#playerNameInput').val() || 'anon'
+             playerName : $('#playerNameInput').val() || 'anon',
          };
          // Send the gameId and playerName to the server
          IO.socket.emit('playerJoinGame', data);
          // Set the appropriate properties for the current player
          App.myRole = 'Player';
          App.Player.myName = data.playerName;
+
+         console.log(App.Player.myName)
+      },
+
+      // Pushing 'waiting for host' msg to Join view
+      updateWaitingScreen: function(data) {
+        console.log(IO.socket.io.engine.id);
+        console.log(data.mySocketId.replace(/^.#+/));
+        if(IO.socket.io.engine.id == data.mySocketId.replace(/^.#+/, '')){
+          App.myRole = 'Player';
+          App.gameId = data.gameId;
+          console.log(App.gameId);
+          // Update host screen
+          $('#waitMsg')
+            .append('<p/>')
+            .text('Waiting for Host to start game...');
+        }
+      },
+
+      // Showing countdown on Player screen
+      gameCountdown : function() {
+          // Prepare the game screen with new HTML
+          App.$gameBoard.html(App.$templateGame);
+          // Hide Scoreboard
+          $('.scores').css('display', 'none');
+          // Begin the on-screen countdown timer
+          var $secondsLeft = $('#countdownTimer');
+          App.countDown( $secondsLeft, 5, function(){
+          });
+      },
+
+      // Behaviour for new round
+      newRiddle : function(data) {
+          console.log('newRiddle called');
+          // Prepare the game screen with new HTML
+          App.$gameBoard.html(App.$templateAnswer);
+          // Insert the round data into the DOM
+          $('#roundCount').text('Round ' + data.round);
+      },
+
+      // Handler for clicking Submit
+      onSubmitClick: function() {
+          var answer = $('#answerInput').val(); // The answer word
+          // Creating data obj to send to server
+          var data = {
+              gameId: App.gameId,
+              playerId: App.mySocketId,
+              answer: answer,
+              round: App.currentRound,
+          }
+          IO.socket.emit('playerAnswer',data);
       },
 
 
     },
 
+    // =======================
+    // Helper Code
+    // =======================
+    countDown : function( $el, startTime, callback) {
+      // Display the starting time on the screen.
+      $el.text(startTime);
+      // Start a 1 second timer
+      var timer = setInterval(countItDown,1000);
+      // Decrement the displayed timer value on each 'tick'
+      function countItDown(){
+        startTime -= 1
+        $el.text(startTime);
 
+        if( startTime <= 0 ){
+          // Stop the timer and do the callback.
+          clearInterval(timer);
+          callback();
+          return;
+        }
+      }
+    },
 
   };
 
   IO.init();
   App.init();
-
-
 
 }($));
--- riddle.js
+++ riddle.js
@@ -11,9 +11,13 @@ exports.initGame = function(request, response){
 
     // Host events
     gameSocket.on('hostCreateNewGame', hostCreateNewGame);
+    gameSocket.on('hostRoomFull', hostPrepareGame);
+    gameSocket.on('countdownFinished', hostStartGame);
+    gameSocket.on('hostNextRound', hostNextRound);
 
     // Player events
     gameSocket.on('playerJoinGame', playerJoinGame);
+    gameSocket.on('playerAnswer', playerAnswer);
 };
 
 
@@ -23,15 +27,43 @@ exports.initGame = function(request, response){
 
 // Triggered by clicking Create
 function hostCreateNewGame() {
+
     // Create a unique Socket.IO Room
     var thisGameId = ( Math.random() * 100000 ) | 0;
 
     // Return the Room ID (gameId) and the socket ID (mySocketId) to the browser client
-    this.emit('newGameCreated', {gameId: thisGameId, mySocketId: this.id});
+    gameSocket.emit('newGameCreated', {gameId: thisGameId, mySocketId: gameSocket.id});
 
     // Join the Room and wait for the players
-    this.join(thisGameId.toString());
+    gameSocket.join(thisGameId.toString());
 };
+
+// Triggered when room is full (4 people)
+function hostPrepareGame(gameId) {
+    var socket = this;
+    var data = {
+        mySocketId : socket.id,
+        gameId : gameId
+    };
+    io.sockets.in(data.gameId).emit('beginNewGame', data);
+};
+
+// Start game!
+function hostStartGame(gameId) {
+    console.log('Game Started.');
+    sendRiddle(0, gameId);
+};
+
+// When correct guess, move on to next round
+function hostNextRound(data) {
+    if(data.round < riddlePool.length ){
+        // Send a new set of words back to the host and players.
+        sendRiddle(data.round, data.gameId);
+    } else {
+        // If the current round exceeds the number of words, send the 'gameOver' event.
+        // io.sockets.in(data.gameId).emit('gameOver',data);
+    }
+}
 
 
 //===================
@@ -39,11 +71,13 @@ function hostCreateNewGame() {
 //===================
 
 function playerJoinGame(data) {
+    console.log('triggered');
     // A reference to the player's Socket.IO socket object
     var socket = this;
     // Look up the room ID in the Socket.IO manager object.
-    var room = gameSocket.manager.rooms["/" + data.gameId];
-    debugger
+    var room = gameSocket.rooms['' + data.gameId];
+    console.log(data.gameId);
+    console.log('room', room);
     // If the room exists...
     if( room != undefined ){
         // attach the socket id to the data object.
@@ -52,9 +86,39 @@ function playerJoinGame(data) {
         socket.join(data.gameId);
         // Emit an event notifying the clients that the player has joined the room.
         io.sockets.in(data.gameId).emit('playerJoinedRoom', data);
-
     } else {
         // Otherwise, send an error message back to the player.
-        this.emit('error',{message: "This room does not exist."} );
+        // this.emit('error', {message: "This room does not exist."} );
     }
+};
+
+// Check answer
+function playerAnswer(data) {
+    // Emit an event with the answer so it can be checked by the 'Host'
+    io.sockets.in(data.gameId).emit('hostCheckAnswer', data);
 }
+
+
+//===================
+// Game Logic
+//===================
+
+function sendRiddle(index, gameId) {
+    var data = {
+      riddle: riddlePool[index][0],
+      question: riddlePool[index][1],
+      answer: riddlePool[index][2],
+      gameId: gameId,
+      round: index + 1
+    }
+    io.sockets.in(data.gameId).emit('newRiddleData', data);
+    console.log(data.gameId);
+}
+
+
+var riddlePool = [
+  ['- Poor people have it. Rich people need it. If you eat it you die. - ', 'What is it?', 'Nothing'],
+  ['- If I drink, I die. If I eat, I am fine. -', 'What am I?', 'Fire'],
+  ['- What word becomes shorter when you add two letters to it? -', '', 'Short'],
+  ['- If I have it, I don’t share it. If I share it, I don’t have it. -', 'What is it?', 'Secrets']
+]
